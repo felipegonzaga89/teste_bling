47101b7bae15fd7cc4b8843cc9dea3cba6a27442
felipegonzaga89 <felipegonzaga@ymail.com>
2024-10-16 14:39:11 -0300

    api restrictions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,7 +1,17 @@
 from rest_framework import viewsets
 from core.models import *
 from .serializers import RequisicoesRecebidasSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 
 class RequisicoesRecebidasViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
+    
     queryset = RequisicoesRecebidas.objects.all()
     serializer_class = RequisicoesRecebidasSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':  # Para o método GET (listar)
+            return [IsAuthenticated()]  # Exige autenticação
+        return super().get_permissions()  # Permite POST sem autenticação
--- teste_bling/settings.py
+++ teste_bling/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'corsheaders',
     'rest_framework',
     
     
@@ -56,7 +58,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'teste_bling.urls'
 
