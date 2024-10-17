c4dcc3755251a5d54cbf0fe70605525fe4cc2f0e
felipegonzaga89 <felipegonzaga@ymail.com>
2024-10-17 16:06:56 -0300

    serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,4 +4,4 @@ from core.models import *
 class RequisicoesRecebidasSerializer(serializers.ModelSerializer):
     class Meta:
         model = RequisicoesRecebidas
-        fields = ['id', 'body']  # Inclua o campo `id` se você quiser retorná-lo também
+        fields = ['id', 'retorno']  # Inclua o campo `id` se você quiser retorná-lo também
