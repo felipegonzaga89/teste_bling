09d6bc220a98e369bbb09e6874123ca8200841a8
felipegonzaga89 <felipegonzaga@ymail.com>
2024-10-17 16:26:13 -0300

    teste viewset

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -11,21 +11,26 @@ class RequisicoesRecebidasViewSet(viewsets.ModelViewSet):
     queryset = RequisicoesRecebidas.objects.all()
     serializer_class = RequisicoesRecebidasSerializer
     
+    # def get_permissions(self):
+    #     if self.action == 'list':  # Para o método GET (listar)
+    #      return [IsAuthenticated()]  # Exige autenticação
+    #     return super().get_permissions()  # Permite POST sem autenticação
+    
     def create(self, request, *args, **kwargs):
         # Cria uma nova instância do serializer com os dados da requisição
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)  # Valida os dados
+        # serializer = self.get_serializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)  # Valida os dados
+        
+        RequisicoesRecebidas.objects.create(retorno=request.data)
+        
 
         # Salva a nova instância no banco de dados
-        self.perform_create(serializer)
+        # self.perform_create(serializer)
 
         # Retorna a resposta com os dados criados
-        return Response(serializer.data, status=status.HTTP_201_CREATED)    
+        return #Response(serializer.data, status=status.HTTP_201_CREATED)    
 
 
 
 
-    def get_permissions(self):
-        if self.action == 'list':  # Para o método GET (listar)
-            return [IsAuthenticated()]  # Exige autenticação
-        return super().get_permissions()  # Permite POST sem autenticação
+   
