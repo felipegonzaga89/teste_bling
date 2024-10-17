7283def9c3ab983de593f4214763819665477c1e
felipegonzaga89 <felipegonzaga@ymail.com>
2024-10-17 16:20:59 -0300

    salvando todo o request

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -10,6 +10,20 @@ class RequisicoesRecebidasViewSet(viewsets.ModelViewSet):
     
     queryset = RequisicoesRecebidas.objects.all()
     serializer_class = RequisicoesRecebidasSerializer
+    
+    def create(self, request, *args, **kwargs):
+        # Cria uma nova instância do serializer com os dados da requisição
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)  # Valida os dados
+
+        # Salva a nova instância no banco de dados
+        self.perform_create(serializer)
+
+        # Retorna a resposta com os dados criados
+        return Response(serializer.data, status=status.HTTP_201_CREATED)    
+
+
+
 
     def get_permissions(self):
         if self.action == 'list':  # Para o método GET (listar)
